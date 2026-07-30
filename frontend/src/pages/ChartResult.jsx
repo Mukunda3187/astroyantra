0fdbc9fa162490b8777f7a878f18fc08fc258b19
Mukunda3187 <cbit051@gmail.com>
2026-07-30 16:53:58 +0530

    Update ChartResult.jsx

--- frontend/src/pages/ChartResult.jsx
+++ frontend/src/pages/ChartResult.jsx
@@ -115,9 +115,6 @@ export default function ChartResult() {
           )}
         </div>
 
-        <div style={{ marginTop: '2.5rem' }}>
-          <button className="btn-secondary" onClick={() => navigate('/')}>New chart</button>
-        </div>
       </div>
     </div>
   );
